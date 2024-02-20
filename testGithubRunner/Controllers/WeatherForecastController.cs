e690f33e31f1e880a189d726a8ecfde1118599a4
Trương Văn Tuân <tuantv@gemvietnam.com>
2024-02-20 14:39:03 +0700

    Update WeatherForecastController.cs

--- testGithubRunner/Controllers/WeatherForecastController.cs
+++ testGithubRunner/Controllers/WeatherForecastController.cs
@@ -41,5 +41,23 @@ namespace testGithubRunner.Controllers
             })
             .ToArray();
         }
+        /*đá
+ đâs
+d
+dá
+d
+sa
+*/
+[HttpGet(Name = "GetWeatherForecast1343")]
+public IEnumerable<WeatherForecast> Get31214()
+{
+    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    })
+    .ToArray();
+}
     }
 }
