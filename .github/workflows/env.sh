108cd6a856d9a9b95c8304c888bf874d30fce200
Trương Văn Tuân <tuantv@gemvietnam.com>
2024-02-26 17:36:28 +0700

    Update env.sh

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function replace_values() {
-  local file_url="$GITHUB_WORKSPACE/appsettings.json"
+  local file_url=$1
   # replace value
 
-  sed -i "s|\"ApiKey\": \".*\"|\"ApiKey\": \"${AZURE_APIKEY}\"|g" "$file_url"
+  sed -i "s|\"ApiKey\": \".*\"|\"ApiKey\": \"${{ env.AZURE_APIKEY }}\"|g" "$file_url"
 
   echo $AZURE_APIKEY | sed 's/./& /g'
 
   cat "$file_url"
 }
 
-replace_values
+replace_values ./test.json
